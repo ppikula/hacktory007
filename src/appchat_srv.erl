6db56da8a7c2a500942633bbc84b9206c1e78b82
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2015-02-07 15:26:13 +0100

    add srv to supervision tree and expose api

--- src/appchat_srv.erl
+++ src/appchat_srv.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -22,36 +22,66 @@
 -define(HOST, <<"xmpp.erlang.pl">>).
 -define(MUC_HOST, <<"muc.xmpp.erlang.pl">>).
 
+-export([send/1, receive_msg/0]).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(Nick) ->
+    %gen_server:start_link({local, ?MODULE}, ?MODULE, [Nick], []).
+    F = fun () ->
+                init([Nick])
+        end,
+    Pid =  spawn_link(F),
+    register(?MODULE,Pid),
+    {ok, Pid}.
+
+
+send(Msg) ->
+    ?MODULE ! {send_message, Msg}.
+
+receive_msg() ->
+    ?MODULE ! {receive_msg, 1, self()},
+    receive
+        R ->
+            R
+    after 5000 ->
+            nomsg
+    end.
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
-init([]) ->
+init([Nick]) ->
     application:ensure_all_started(escalus),
-    Cfg = user_spec(<<"case">>, <<"res1">>),
+    Cfg = user_spec(Nick, <<"res1">>),
     {ok, Client, _, _} = escalus_connection:start(Cfg),
-    AllowMessage = fun escalus_pred:is_groupchat_message/1,
-   % escalus_connection:set_filter_predicate(Client, AllowMessage),
     send_presence_available(Client),
-    escalus:send(Client, stanza_muc_enter_room(<<"hackatory">>, <<"case">>)),
-    GC = escalus_stanza:groupchat_to(<<"hackatory@muc.xmpp.erlang.pl">>, <<"test">>),
-    io:format("~p", [GC]),
-    escalus:send(Client, GC),
+    escalus:send(Client, stanza_muc_enter_room(<<"hackatory">>, Nick)),
+    loop(Client).
     %%Allow presence stanza only
-    {ok, #state{client=Client}}.
+    %{ok, #state{client=Client}}.
 
-handle_call({send_message, M}, From, State) ->
+loop(Client) ->
+    receive
+        {send_message, M} ->
+            GC = escalus_stanza:groupchat_to(<<"hackatory@muc.xmpp.erlang.pl">>, M),
+            escalus:send(Client, GC);
+        {receive_msg, C, From} ->
+            S = escalus_client:wait_for_stanzas(Client, 1, 4000),
+            From ! wait_for_message(Client, S)
+     end,
+    loop(Client).
+
+handle_call({send_message, M}, From, #state{client=Client} = State) ->
+    GC = escalus_stanza:groupchat_to(<<"hackatory@muc.xmpp.erlang.pl">>, M),
+    escalus:send(Client, GC),
     {reply, ok, State};
-handle_call({receive_message, M}, From, State) ->
-    {reply, ok, State};
+handle_call({receive_message, Count}, From, #state{client = Client} =  State) ->
+    S = escalus_client:wait_for_stanzas(Client, 1, 4000),
+    {reply, S, State};
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
@@ -71,6 +101,13 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+wait_for_message(_Client, [#xmlel{name = <<"message">>} = Stanza]) ->
+    Stanza;
+wait_for_message(_, []) -> nomsg;
+wait_for_message(Client, _ ) ->
+    S = escalus_client:wait_for_stanzas(Client, 1),
+    wait_for_message(Client, S).
+
 
 user_spec(ProfileId, Res) ->
     [ {username, ProfileId},
--- src/appchat_sup.erl
+++ src/appchat_sup.erl
@@ -23,5 +23,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, { {one_for_one, 5, 10}, [{appchat_srv, {appchat_srv, start_link, [<<"case">>]},
+                permanent, brutal_kill, worker, [aapchat_srv]}]} }.
 
